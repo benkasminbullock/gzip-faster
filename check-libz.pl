d14a622111f67aa1908a3d9a693b08cd6960dc94
Ben Bullock <benkasminbullock@gmail.com>
2015-11-05 11:06:34 +0900

    Add better comments in check-libz.pl.

--- check-libz.pl
+++ check-libz.pl
@@ -1,4 +1,17 @@
 #!/usr/bin/env perl
+
+# This is a script for debugging installation problems for the
+# underlying compression library zlib. Instructions for running this
+# script may be found in lib/Gzip/Faster.pod. It's easy to read it
+# like this:
+#
+# perldoc lib/Gzip/Faster.pod
+#
+# This script is intended to be run by a user to analyze the problem
+# with installation on their computer. The script will be extended as
+# various issues are found and resolved. Please use this script to
+# communicate with the module author at <bkb@cpan.org>.
+
 use warnings;
 use strict;
 use utf8;
@@ -8,10 +21,13 @@ BEGIN {
     use lib "$Bin/inc";
     use Devel::CheckLib;
 };
-#my $verbose = 1;
 
 # Flush output so that we can capture the subprocess stderr correctly.
+
 $| = 1;
+
+# Make 100% sure that the user has run the script in the correct
+# directory.
 
 if (! -f "$Bin/inc/Devel/CheckLib.pm") {
     line ();
@@ -20,6 +36,9 @@ if (! -f "$Bin/inc/Devel/CheckLib.pm") {
     line ();
     exit 1;
 }
+
+# Get the user's operating system.
+
 line ();
 my @info = uname ();
 msg ("Your system info: @info");
@@ -42,6 +61,10 @@ else {
 
 exit;
 
+# All of this program's output goes via line and msg. Their
+# "file:line:" format ensures that we can distinguish this program's
+# output from the Devel::CheckLib output or other subprocess output.
+
 sub line
 {
     my (undef, $file, $line) = caller ();
@@ -50,9 +73,6 @@ sub line
 
 sub msg
 {
-#    if ($verbose) {
-#	if ($debug) {
-	my (undef, $file, $line) = caller ();
-	print "$file:$line: @_\n";
-#    }
+    my (undef, $file, $line) = caller ();
+    print "$file:$line: @_\n";
 }
