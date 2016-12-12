44f1f68ef94528804f38b911661362d3090313e1
Ben Bullock <benkasminbullock@gmail.com>
2016-12-13 08:49:34 +0900

    Improve the documentation, especially new and other methods

--- build.pl
+++ build.pl
@@ -4,5 +4,6 @@ use strict;
 use Perl::Build;
 perl_build (
     make_pod => './make-pod.pl',
+    clean => './clean.pl',
 );
 exit;
--- examples/synopsis.pl
+++ examples/synopsis.pl
@@ -4,9 +4,11 @@ use strict;
 # Make a random input string
 my $input = join '', map {int (rand (10))} 0..0x1000;
 use Gzip::Faster;
+# Compress the random string.
 my $gzipped = gzip ($input);
+# Uncompress it again.
 my $roundtrip = gunzip ($gzipped);
-if ($roundtrip ne $input) { die; }
+# Put it into a file.
 gzip_to_file ($input, 'file.gz');
+# Retrieve it again from the file.
 $roundtrip = gunzip_file ('file.gz');
-if ($roundtrip ne $input) { die; }
