d16dec053e63c58aecbe6808d7f73f0cda3c40e8
Ben Bullock <benkasminbullock@gmail.com>
2017-12-29 10:30:12 +0900

    Make error message creation more consistent.

--- deploy.pl
+++ deploy.pl
@@ -21,3 +21,4 @@ do_system ("faq-module-build.pl $tf");
 do_system ("lemoda-module-build.pl $tf");
 do_system ("kanji-module-build.pl $tf");
 do_system ("nxmnpg-module-build.pl $tf");
+do_system ("qrpng-module-build.pl $tf");
