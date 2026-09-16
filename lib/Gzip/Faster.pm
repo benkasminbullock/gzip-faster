5ef75daf6583b5e941bcb40b0e3e580270ae3936
Ben Bullock <benkasminbullock@gmail.com>
2026-09-17 07:30:47 +0900

    Version to 0.22_02 and try to fix Clang nuisance problems

--- lib/Gzip/Faster.pm
+++ lib/Gzip/Faster.pm
@@ -7,7 +7,7 @@ our @EXPORT = qw/gzip gunzip gzip_file gunzip_file gzip_to_file/;
 our @EXPORT_OK = qw/deflate inflate deflate_raw inflate_raw/;
 our %EXPORT_TAGS = ('all' => [@EXPORT, @EXPORT_OK]);
 use Carp;
-our $VERSION = '0.22_01';
+our $VERSION = '0.22_02';
 require XSLoader;
 XSLoader::load ('Gzip::Faster', $VERSION);
 
